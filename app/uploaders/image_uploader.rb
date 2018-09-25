021cbba93579c58f034a4cf892e8ce00239adac4
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-09-25 22:54:28 +0900

    cssで見た目修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
   storage :file
-  process :resize_to_limit => [250, 250]
+  process :resize_to_limit => [300, 300]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
