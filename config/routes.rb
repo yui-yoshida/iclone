70c9aa4c7189d1f9fc42d60b70e3a67c42b1c223
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-09-29 11:51:59 +0900

    syuusei

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'sessions/new'
+  # root to: 'sessions/new'
 
   resources :pictures do
     collection do
       post :confirm
     end
   end
-  get '/favorites/:id', to: 'users#fav_pictures'
+  # get '/favorites/:id', to: 'users#fav_pictures'
   resources :users
   resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
