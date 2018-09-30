6f5cccd8e1ca90d1d3d114b8b1ba8e89640ad466
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-09-30 22:23:16 +0900

    メールが正しく送られるように修正

--- app/mailers/create_mailer.rb
+++ app/mailers/create_mailer.rb
@@ -3,6 +3,6 @@ class CreateMailer < ApplicationMailer
   def create_mail(picture)
     @picture = picture
 
-    mail to: @picture.email, subject: "お問い合わせ確認メール"
+    mail to: @picture.user.email, subject: "お問い合わせ確認メール"
   end
 end
